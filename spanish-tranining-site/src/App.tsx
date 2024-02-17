c3c386528783b9ed9574dd6c70d126ca45883c6b
erikisakss <erik@isaksson.biz>
2024-02-17 17:30:51 +0100

    Update App.tsx

--- spanish-tranining-site/src/App.tsx
+++ spanish-tranining-site/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen  p-4">
-        <h1 className="text-4xl text-gray-800 font-bold mb-8 dark:text-white-800">Spanish Training Site</h1>
+        <h1 className="text-4xl text-gray-800 font-bold mb-8 dark:text-white-800">Spanish Training Site!</h1>
         {activeQuiz && (
            <button onClick={() => setActiveQuiz(activeQuiz === 'write-translation' ? 'multiple-choice' : 'write-translation')} className='mb-4 bg-orange-500 text-white font-semibold py-2 px-4 rounded hover:bg-orange-700 transition duration-300'>
            Switch Quiz
